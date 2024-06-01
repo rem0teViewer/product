432769fb9a07c8a964080ad594b06d03ce049e7a
Andrzej <sheo.13@hotmail.com>
2024-06-01 19:03:51 +0300

    ISSUE-3

---
+++ e2e/src/test/java/org/iprody/e2e/ProductServiceTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iprody.e2e;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceTestingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
