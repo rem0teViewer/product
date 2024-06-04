29c9bd67729f3672a45127c0de78e77ecf9bb227
Andrzej <sheo.13@hotmail.com>
2024-06-04 18:52:40 +0300

    6

---
+++ e2e/src/test/java/org/iprody/e2e/CucumberTest.java
@@ -0,0 +1,12 @@
+package org.iprody.e2e;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        features = "src/test/resources/features",
+        plugin = {"json:target/cucumber.json", "html:target/cucumber-html-report"})
+public class CucumberTest {
+}
